832715ec5ad53be347c3957d571fefee513dc33d
Pavel Ilchishin <p.ilchishin@gmail.com>
2025-07-09 11:43:59 +0300

    Изменил PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -686,9 +686,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  22.64
+    #define DEFAULT_Ki   2.38
+    #define DEFAULT_Kd 53.92
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
